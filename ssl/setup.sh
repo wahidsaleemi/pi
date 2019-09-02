705eba6d16cc8197945238ca205e957e499712d7
Wahid Saleemi <onewahid@live.com>
2019-09-02 12:29:55 -0500

    only add smartrenew to cront

--- ssl/setup.sh
+++ ssl/setup.sh
@@ -1,8 +1,7 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "0 22 * * * /home/pi/ssl/smartrenew.sh" >> mycron
-echo "0 0 1 * * /home/pi/ssl/renew.sh 2>> /var/log/acme_tiny.log" >> mycron
+echo "0 22 * * * /home/pi/ssl/smartrenew.sh 2>> /var/log/acme_tiny.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
@@ -12,4 +11,4 @@ sudo touch /var/log/acme_tiny.log
 sudo chown acme:acme /var/log/acme_tiny.log
 
 #make renew files executable
-chmod +x renew.sh smartrenew.sh
+chmod +x ~/pi/ssl/renew.sh ~/pi/ssl/smartrenew.sh
